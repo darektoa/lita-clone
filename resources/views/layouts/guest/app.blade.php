b4e9619e8c0288d3a3a4ff5f32d89097328c3311
darektoa <darektoa@gmail.com>
2021-09-30 22:34:53 +0700

    Added: Guest base layout

---
+++ resources/views/layouts/guest/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title>Lita - Play with Gamer Girls & Pro Players</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Abdul">
+    <meta name="description" content="Meet new gamer friends, voice chat and play games together! Mobile Legends, PUBG, Free Fire, League of Legends, Call of Duty, Among Us and much more!">
+    <meta name="theme-color" content="#FFF">
+    <meta name="title" content="Lita - Play with Gamer Girls & Pro Players">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:title" content="Lita - Play with Gamer Girls & Pro Players">
+    <meta property="og:description" content="Meet new gamer friends, voice chat and play games together! Mobile Legends, PUBG, Free Fire, League of Legends, Call of Duty, Among Us and much more!">
+    <meta property="og:image" content="{{ asset('favicon.ico') }}">
+
+    @yield('head')
+
+  </head>
+  <body>
+
+    @yield('body')
+
+    @yield('scripts')
+
+  </body>
+</html>
