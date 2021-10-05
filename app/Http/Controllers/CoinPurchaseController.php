f49f2298f1b70269aa54c24608bdc760febba33a
darektoa <darektoa@gmail.com>
2021-10-05 12:39:33 +0700

    Added: Spliting between admin topup and player topup

--- app/Http/Controllers/CoinPurchaseController.php
+++ app/Http/Controllers/CoinPurchaseController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use Alert;
 use App\Models\{Coin, CoinPurchase};
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+use function PHPUnit\Framework\throwException;
 
 class CoinPurchaseController extends Controller
 {
@@ -16,18 +20,37 @@ class CoinPurchaseController extends Controller
 
     
     public function store(Request $request) {
+        $user = auth()->user();
         $coin = Coin::find($request->coin);
 
-        if($coin) {
-            CoinPurchase::create([
-                'player_id' => auth()->user()->player->id,
-                'coin_id' => $coin->id
-            ]);
-            Alert::success('Success');
+        try{
+            if(!$coin) throw new Exception('Invalid Coin');
+            if($user->admin) $this->storeAdmin($request);
+            if($user->player) $this->storePlayer($request);
+            Alert::success('Success', 'Topup Successfully');
+            return back();
+        }catch(Exception $err) {
+            $errMessages = $err->getMessage();
+            Alert::error('Failed', $errMessages);
             return back();
         }
+    }
 
-        Alert::error('Failed');
-        return back();
+
+    private function storeAdmin(Request $request) {
+        CoinPurchase::create([
+            'player_id' => $request->player->id,
+            'admin_id' => auth()->user()->admin->id,
+            'coin_id' => $request->coin,
+            'status' => 2,
+        ]);
+    }
+
+
+    private function storePlayer(Request $request) {
+        CoinPurchase::create([
+            'player_id' => auth()->user()->player->id,
+            'coin_id' => $request->coin
+        ]);
     }
 }
