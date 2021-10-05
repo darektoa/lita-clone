fb165bdad9379eac1f9e00d793c37dcda48d0985
darektoa <darektoa@gmail.com>
2021-10-05 16:56:24 +0700

    Added: Total data by user login in dashboard page

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\CoinPurchase;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function __invoke(Request $request)
     {
-        return view('pages.general.dashboard');
+        $user = auth()->user();
+        $total = collect([]);
+        $coinPurchase = null;
+
+        if($user->admin) $coinPurchase = CoinPurchase::where('admin_id', $user->admin->id);
+        else $coinPurchase = CoinPurchase::where('player_id', $user->player->id);
+        
+        $total->put('approved', $coinPurchase->where('status', 2)->count());
+        $total->put('pending',  $coinPurchase->where('status', 0)->count());
+        $total->put('rejected', $coinPurchase->where('status', 1)->count());
+
+        return view('pages.general.dashboard', compact('total'));
     }
 }
--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
   <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins"/>
-  <x-dashboard.info-box title="Topup Success" value="3" color="success" icon="fa-check"/>
-  <x-dashboard.info-box title="Topup Pending" value="5" color="warning" icon="fa-clock"/>
-  <x-dashboard.info-box title="Topup Failed" value="1" color="danger" icon="fa-ban"/>
+  <x-dashboard.info-box title="Topup Success" value="{{ $total['approved'] }}" color="success" icon="fa-check"/>
+  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock"/>
+  <x-dashboard.info-box title="Topup Failed" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban"/>
 </div>
