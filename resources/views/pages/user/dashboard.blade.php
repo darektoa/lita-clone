5ec5ba39b59daf8f1faed8d3a9aab0a48f967199
darektoa <darektoa@gmail.com>
2021-10-06 20:52:58 +0700

    Edited: Color of coin icon in user dashboard info box

--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-  <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins text-gray-300"/>
+  <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins text-warning"/>
   <x-dashboard.info-box title="Topup Success" value="{{ $total['approved'] }}" color="success" icon="fa-check text-gray-300"/>
   <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock text-gray-300"/>
   <x-dashboard.info-box title="Topup Failed" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban text-gray-300"/>
