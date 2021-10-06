2a2eadb578b4880c8882d0193e065a89087c45cd
darektoa <darektoa@gmail.com>
2021-10-06 22:12:07 +0700

    Added: Register API route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -30,4 +30,46 @@ class AuthController extends Controller
             return response()->json(['message'=> 'invalid login'], 401);
         }
     }
+    
+
+
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'first_name'        => 'bail|required|alpha|min:2|max:20',
+            'last_name'         => 'required|alpha|min:2|max:20',
+            'email'             => 'required|email|unique:users',
+            'password'          => 'required|min:5|max:16',
+            'password_confirm'  => 'required|same:password'
+        ]);
+
+        $errors = $validator->errors();
+            // dd($errors);
+        if($validator->fails()) {
+            return response()->json([
+                'message' => 'invalid field',
+                'errors' => $errors['email']
+            ], 422);
+        }
+
+        // Create Account
+        $emailName = explode('@', $request->email)[0];
+        $user = User::create([
+            'first_name' => $request->first_name,
+            'last_name'  => $request->last_name,
+            'username'   => UsernameHelper::make($emailName),
+            'email'      => $request->email,
+            'password'   => Hash::make($request->password)
+        ]);
+
+        // Login User
+        $userId = $user->id;
+        $loginToken = LoginToken::create([
+            'user_id' => $userId,
+            'token'   => Hash::make($userId),
+        ]);
+
+        return response()->json([
+            'token'=> $loginToken->token
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
