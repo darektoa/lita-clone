a97a46649415e0629789262b72159ffba97153ee
darektoa <darektoa@gmail.com>
2021-10-06 20:51:24 +0700

    Edited: Dashboard info-box component with customable icon color

--- resources/views/components/dashboard/info-box.blade.php
+++ resources/views/components/dashboard/info-box.blade.php
@@ -9,7 +9,7 @@
                     <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $value }}</div>
                 </div>
                 <div class="col-auto">
-                    <i class="fas {{ $icon }} fa-2x text-gray-300"></i>
+                    <i class="fas {{ $icon }} fa-2x"></i>
                 </div>
             </div>
         </div>
--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
-  <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins"/>
-  <x-dashboard.info-box title="Topup Success" value="{{ $total['approved'] }}" color="success" icon="fa-check"/>
-  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock"/>
-  <x-dashboard.info-box title="Topup Failed" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban"/>
+  <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins text-gray-300"/>
+  <x-dashboard.info-box title="Topup Success" value="{{ $total['approved'] }}" color="success" icon="fa-check text-gray-300"/>
+  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock text-gray-300"/>
+  <x-dashboard.info-box title="Topup Failed" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban text-gray-300"/>
 </div>
