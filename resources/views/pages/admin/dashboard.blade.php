d0dbb515bc6bfdbbd33687c92d06c734d0bd6b5a
darektoa <darektoa@gmail.com>
2021-10-06 21:11:46 +0700

    Edited: Color of users icon in admin dashboard info box

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
-  <x-dashboard.info-box title="Users" value="{{ \App\Models\User::count() }}" color="info" icon="fa-users  text-gray-300"/>
-  <x-dashboard.info-box title="Topup Approved" value="{{ $total['approved'] }}" color="success" icon="fa-check  text-gray-300"/>
-  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock  text-gray-300"/>
-  <x-dashboard.info-box title="Topup Rejected" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban  text-gray-300"/>
+  <x-dashboard.info-box title="Users" value="{{ \App\Models\User::count() }}" color="info" icon="fa-users text-info"/>
+  <x-dashboard.info-box title="Topup Approved" value="{{ $total['approved'] }}" color="success" icon="fa-check text-gray-300"/>
+  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock text-gray-300"/>
+  <x-dashboard.info-box title="Topup Rejected" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban text-gray-300"/>
 
 </div>
