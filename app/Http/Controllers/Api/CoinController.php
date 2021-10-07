d756dfa3cba429eefe4a22ea3060992f615b42f2
darektoa <darektoa@gmail.com>
2021-10-07 23:26:03 +0700

    Added: Coin histories API, Coin pending histories API, Coin approved histories API, Coin failed histories API, Coin canceled histories API

---
+++ app/Http/Controllers/Api/CoinController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\CoinPurchase;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CoinController extends Controller
+{
+    public function index(Request $request) {
+        $user = auth()->user();
+        $purchases = CoinPurchase::withTrashed();
+        $statusId = (int)$request->status;
+
+        if($statusId >= 0 && $statusId < 5) $purchases = $purchases->where('status', $statusId);
+        if($user->admin) $purchases = $purchases->latest();
+        else $purchases = $purchases->where('player_id', $user->player->id);
+
+        $purchases = $purchases
+            ->paginate(10);
+        
+        return response()->json(['data' => $purchases]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth.api'])->group(function() {
+    Route::get('/coins', [CoinController::class, 'index']);
+});
