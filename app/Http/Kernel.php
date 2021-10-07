fa0c3e3f497335e3f43acb4ca3d8728f32d276fb
darektoa <darektoa@gmail.com>
2021-10-07 22:23:38 +0700

    Added: AuthAPI middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,6 +56,7 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'admin' => \App\Http\Middleware\IsAdmin::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'auth.api' => \App\Http\Middleware\Api\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
---
+++ app/Http/Middleware/Api/Authenticate.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware\Api;
+
+use App\Models\LoginToken;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Authenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!$request->has('token'))
+            return response()->json(['message' => 'Invalide token field'], 422);
+
+        $token = $request->token;
+        $loginToken = LoginToken::where('token', $token)->first();
+        if(!$loginToken) return response()->json(['message' => 'Unauthorized token'], 201);
+
+        Auth::login($loginToken->user);
+        return $next($request);
+    }
+}
