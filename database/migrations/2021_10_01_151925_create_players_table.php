e65ffd3ede4a2c3657dc98a24552a8a7da368e5b
darektoa <darektoa@gmail.com>
2021-10-13 22:03:49 +0700

    Fixed: Players migration at rate field

--- database/migrations/2021_10_01_151925_create_players_table.php
+++ database/migrations/2021_10_01_151925_create_players_table.php
@@ -17,7 +17,7 @@ class CreatePlayersTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->bigInteger('coin')->default(0);
-            $table->float('rate', 1, 2);
+            $table->float('rate', 3, 2)->default(0);
             $table->text('voice')->nullable();
             $table->smallInteger('is_pro_player')->default(0);
             $table->timestamps();
