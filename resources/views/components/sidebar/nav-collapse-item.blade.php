ce9e283db13b9126151b82db3bf3cf53e7b6510e
darektoa <darektoa@gmail.com>
2021-10-13 19:58:50 +0700

    Fixed: Nav collapse item component

--- resources/views/components/sidebar/nav-collapse-item.blade.php
+++ resources/views/components/sidebar/nav-collapse-item.blade.php
@@ -1,10 +1,10 @@
 <li class="nav-item {{ $active }}">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities{{ $name }}"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw {{ $icon }}"></i>
         <span>{{ $name }}</span>
     </a>
-    <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+    <div id="collapseUtilities{{ $name }}" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
 
