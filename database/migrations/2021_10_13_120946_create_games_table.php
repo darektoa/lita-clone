2111a2f6360a74fca4527c0de2b98ccbecd718c2
darektoa <darektoa@gmail.com>
2021-10-14 19:07:33 +0700

    Added: Soft deletes in Game model and migration

--- database/migrations/2021_10_13_120946_create_games_table.php
+++ database/migrations/2021_10_13_120946_create_games_table.php
@@ -16,6 +16,7 @@ class CreateGamesTable extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
