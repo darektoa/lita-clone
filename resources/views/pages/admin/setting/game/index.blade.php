d89a6f0bc89aecafeff94294a902700dc5e288b1
darektoa <darektoa@gmail.com>
2021-10-14 11:03:05 +0700

    Added: Add game modal in game setting admin page

--- resources/views/pages/admin/setting/game/index.blade.php
+++ resources/views/pages/admin/setting/game/index.blade.php
@@ -5,7 +5,7 @@
 	<div class="card shadow mb-4">
 		<div class="card-header py-3 d-flex align-items-center">
 			<h6 class="m-0 font-weight-bold text-primary">Game List</h6>
-			<a class="btn btn-primary ml-4" href="{{ route('topup.index') }}">Add Game</a>
+			<button type="button" class="btn btn-primary ml-4" data-toggle="modal" data-target="#addGameModal">Add Game</button>
 			<form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" method="get"
 				action="">
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" name="keyword"
@@ -14,6 +14,32 @@
 			</form>
 		</div>
 		<div class="card-body table-responsive" style="min-height: 400px">
+
+			<div class="modal fade" id="addGameModal" tabindex="-1" role="dialog" aria-labelledby="addGameModalLabel" aria-hidden="true">
+				<div class="modal-dialog" role="document">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="addGameModalLabel">Add Game</h5>
+							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<div class="modal-body">
+							<form>
+								<div class="form-group">
+									<label for="recipient-name" class="col-form-label">Game Name:</label>
+									<input type="text" class="form-control" id="game-name">
+								</div>
+							</form>
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+							<button type="submit" class="btn btn-primary">Submit</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
 			<table class="table table-hover " id="dataTable" width="100%" cellspacing="0">
 				<thead>
 					<tr>
