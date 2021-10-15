d67f494ad6736ebab81950d25b7dfe3d41c54c2d
darektoa <darektoa@gmail.com>
2021-10-15 15:21:39 +0700

    Fixed: Id nav collapse item component

--- resources/views/components/sidebar/nav-collapse-item.blade.php
+++ resources/views/components/sidebar/nav-collapse-item.blade.php
@@ -1,10 +1,10 @@
 <li class="nav-item {{ $active }}">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities{{ $name }}"
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities{{ Str::replace(' ', '', $name) }}"
         aria-expanded="true" aria-controls="collapseUtilities">
         <i class="fas fa-fw {{ $icon }}"></i>
         <span>{{ $name }}</span>
     </a>
-    <div id="collapseUtilities{{ $name }}" class="collapse" aria-labelledby="headingUtilities"
+    <div id="collapseUtilities{{ Str::replace(' ', '', $name) }}" class="collapse" aria-labelledby="headingUtilities"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
 
