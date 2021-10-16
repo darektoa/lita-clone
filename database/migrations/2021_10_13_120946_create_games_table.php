91b8d4f6702db1593aed298fa77499f34cd9932a
darektoa <darektoa@gmail.com>
2021-10-16 17:51:06 +0700

    Added: Icon field in games migration

--- database/migrations/2021_10_13_120946_create_games_table.php
+++ database/migrations/2021_10_13_120946_create_games_table.php
@@ -15,6 +15,7 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->id();
+            $table->text('icon');
             $table->string('name', 100);
             $table->softDeletes();
             $table->timestamps();
