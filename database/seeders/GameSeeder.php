721380cd5362796ae0c496ded139a92cf0ff8f6c
darektoa <darektoa@gmail.com>
2021-10-18 16:27:59 +0700

    Added: Default icon path in game seeder

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -9,34 +9,35 @@ class GameSeeder extends Seeder
 {
     public function run()
     {
-        $roles = [
-            'mobileLegends' => ['Assasin', 'Fighter', 'Mage', 'Marksman', 'Support', 'Tank'],
-            'freeFire'      => ['Flanker', 'Hitscan', 'Leader', 'Sniper', 'Support', 'Survivor', 'Tracker'],
-            'pubgMobile'    => ['Leader', 'Rusher', 'Scout', 'Sniper', 'Support'],
+        $games = [
+            [
+                'name'  => 'Mobile Legends',
+                'icon'  => 'images/game-icons/c2042d83-236c-4e8f-86b9-5af295c11eda.png',
+                'tiers' => ['Warrior', 'Elite', 'Master', 'Grand Master', 'Epic', 'Legend', 'Mythic', 'Mythical Glory'],
+                'roles' => ['Assasin', 'Fighter', 'Mage', 'Marksman', 'Support', 'Tank'],
+            ], [
+                'name'  => 'Free Fire',
+                'icon'  => 'images/game-icons/ac6256da-0a0d-4ef7-a58e-b5ed59842252.png',
+                'tiers' => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Heroic'],
+                'roles' => ['Flanker', 'Hitscan', 'Leader', 'Sniper', 'Support', 'Survivor', 'Tracker'],
+            ], [
+                'name'  => 'PUBG Mobile',
+                'icon'  => 'images/game-icons/be0b28ea-26c0-43db-92d2-e52df57107ad.png',
+                'tiers' => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Crown', 'Ace', 'Conqueror'],
+                'roles' => ['Leader', 'Rusher', 'Scout', 'Sniper', 'Support'],
+            ],
         ];
 
-        $tiers = [
-            'mobileLegends' => ['Warrior', 'Elite', 'Master', 'Grand Master', 'Epic', 'Legend', 'Mythic', 'Mythical Glory'],
-            'freeFire'      => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Heroic'],
-            'pubgMobile'    => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Crown', 'Ace', 'Conqueror'],
-        ];
 
+        
+        foreach($games as $game) {
+            $model = Game::create([
+                'name' => $game['name'],
+                'icon' => $game['icon'],
+            ]);
 
-        // Mobile Legends
-        $game = Game::create(['name' => 'Mobile Legends', 'icon' => '/']);
-        foreach($tiers['mobileLegends'] as $tier) $game->gameTiers()->create(['name' => $tier]);
-        foreach($roles['mobileLegends'] as $role) $game->gameRoles()->create(['name' => $role]);
-        
-
-        // Free Fire
-        $game = Game::create(['name' => 'Free Fire', 'icon' => '/']);
-        foreach($tiers['freeFire'] as $tier) $game->gameTiers()->create(['name' => $tier]);
-        foreach($roles['freeFire'] as $role) $game->gameRoles()->create(['name' => $role]);
-        
-        
-        // PUBG Mobile
-        $game = Game::create(['name' => 'PUBG Mobile', 'icon' => '/']);
-        foreach($tiers['pubgMobile'] as $tier) $game->gameTiers()->create(['name' => $tier]);
-        foreach($roles['pubgMobile'] as $role) $game->gameRoles()->create(['name' => $role]);
+            foreach($game['tiers'] as $tier) $model->gameTiers()->create(['name' => $tier]);
+            foreach($game['roles'] as $role) $model->gameRoles()->create(['name' => $role]);
+        }
     }
 }
