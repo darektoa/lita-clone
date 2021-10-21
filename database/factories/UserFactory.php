0fc44e071537a0589e72f432a65e8acb0d876f3d
darektoa <darektoa@gmail.com>
2021-10-22 00:10:23 +0700

    Added: Faker in bio field of user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,13 +14,14 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'first_name' => $this->faker->firstName('male'),
-            'last_name' => $this->faker->lastName(),
-            'username' => $this->faker->unique()->userName(),
-            'email' => $this->faker->unique()->companyEmail(),
+            'first_name'        => $this->faker->firstName('male'),
+            'last_name'         => $this->faker->lastName(),
+            'username'          => $this->faker->unique()->userName(),
+            'email'             => $this->faker->unique()->companyEmail(),
+            'bio'               => $this->faker->realText(255),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
         ];
     }
 
