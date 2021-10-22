915c05bac68cc3f61e63a234fb2b712ecd66fd5f
darektoa <darektoa@gmail.com>
2021-10-22 15:33:48 +0700

    Added: Status and message in user list API route

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,10 @@ class UserController extends Controller
     public function index() {
         $users = User::all();
 
-        return response()->json(['data' => $users]);
+        return response()->json([
+            'status'    => 200,
+            'message'   => 'OK',
+            'data'      => UserResource::collection($users)
+        ]);
     }
 }
