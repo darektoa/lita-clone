8b4411b29b7272d2e6045e089315215bad3bdb3f
darektoa <darektoa@gmail.com>
2021-10-22 17:20:42 +0700

    Added: Balance transactions table, migration and model

---
+++ database/migrations/2021_10_01_151928_create_balance_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBalanceTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('balance_transactions', function (Blueprint $table) {
+            $table->uuid('uuid');
+            $table->foreignId('sender_id')->nullable()->constrained('players');
+            $table->foreignId('receiver_id')->constrained('players');
+            $table->smallInteger('type');
+            $table->bigInteger('coin')->unsigned();
+            $table->bigInteger('balance');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('balance_transactions');
+    }
+}
