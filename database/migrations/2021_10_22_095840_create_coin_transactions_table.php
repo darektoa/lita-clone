c2636c0792f31a4e1baabbf9466ab8f3bd71f023
darektoa <darektoa@gmail.com>
2021-10-22 17:15:38 +0700

    Added: Coin transactions table, migration and model

---
+++ database/migrations/2021_10_22_095840_create_coin_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoinTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coin_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->foreignId('sender_id')->nullable()->constrained('players');
+            $table->foreignId('receiver_id')->constrained('players');
+            $table->smallInteger('type');
+            $table->bigInteger('coin')->unsigned();
+            $table->bigInteger('balance');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coin_transactions');
+    }
+}
