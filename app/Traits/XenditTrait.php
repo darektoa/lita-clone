fb8859c587c20fbe119f43053878f1f0a91302bb
darektoa <darektoa@gmail.com>
2021-10-23 23:09:24 +0700

    Added: Xendit trait

---
+++ app/Traits/XenditTrait.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Traits;
+
+use Xendit\Invoice as XenditInvoice;
+
+trait XenditTrait{
+  static public function invoice($transaction) {
+    $mode     = env('XENDIT_MODE');
+    $ownerId  = $mode === 'production' ? env('XENDIT_OWNER_ID') : env('XENDIT_OWNER_ID_DEV');
+    
+    $invoice = XenditInvoice::create([
+      'for-user-id'   => $ownerId,
+      'external_id'   => $transaction->uuid,
+      'amount'        => $transaction->balance,
+      'description'   => $transaction->description ?? 'Deskripsi',
+      'payer_email'   => $transaction->receiver->email,
+      'fixed_va'      => true,
+      'customer'      => [
+        'given_names'   => $transaction->receiver->first_name,
+        'email'         => $transaction->receiver->email,
+      ]
+    ]);
+
+    return $invoice;
+  }
+}
