556a1bcf90b1aacf4ae5a40d58db795dc8afeb61
darektoa <darektoa@gmail.com>
2021-10-23 14:54:31 +0700

    Added: App settings table and migration

--- database/migrations/2021_10_23_043629_create_app_settings_table.php
+++ database/migrations/2021_10_23_043629_create_app_settings_table.php
@@ -17,6 +17,7 @@ class CreateAppSettingsTable extends Migration
             $table->id();
             $table->smallInteger('company_revenue_percentage')->default(30);
             $table->bigInteger('coin_conversion')->unsigned();
+            $table->text('terms_rules');
             $table->timestamps();
         });
     }
