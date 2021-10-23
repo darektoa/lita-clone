57419a80cddd9f87b40afdcc5b88bd470fd055ea
darektoa <darektoa@gmail.com>
2021-10-23 14:52:22 +0700

    Added: Xendit library

---
+++ database/migrations/2021_10_23_043629_create_app_settings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_settings', function (Blueprint $table) {
+            $table->id();
+            $table->smallInteger('company_revenue_percentage')->default(30);
+            $table->bigInteger('coin_conversion')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_settings');
+    }
+}
