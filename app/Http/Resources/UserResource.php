d45a170be5a4dca13f0d075959c00a5eb82d0d09
darektoa <darektoa@gmail.com>
2021-10-28 22:51:30 +0700

    Added: Gender API resource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -25,7 +25,7 @@ class UserResource extends JsonResource
             'cover_photo'   => StorageHelper::url($this->cover_photo),
             'birthday'      => $this->birthday,
             'bio'           => $this->bio,
-            'gender'        => $this->when($this->gender, $this->gender),
+            'gender'        => $this->when($this->gender, new GenderResource($this->gender)),
             'player'        => $this->when($this->player, $this->player),
             'admin'         => $this->when($this->admin, $this->admin)
         ];
