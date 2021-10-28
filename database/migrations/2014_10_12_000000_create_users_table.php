ccf02481616ed3eeef104fe3c86720356845f7c9
darektoa <darektoa@gmail.com>
2021-10-28 22:13:13 +0700

    Added: Gender id field in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('gender_id')->nullable()->constrained();
             $table->string('first_name', 15);
             $table->string('last_name', 15);
             $table->string('username', 30)->unique();
