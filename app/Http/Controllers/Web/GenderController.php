b2bde424a787546a85768d1ae027ab989f9bc358
darektoa <darektoa@gmail.com>
2021-10-28 18:35:12 +0700

    Added: Gender list in admin setting page

--- app/Http/Controllers/Web/GenderController.php
+++ app/Http/Controllers/Web/GenderController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gender;
 use Illuminate\Http\Request;
 
 class GenderController extends Controller
 {
     public function index() {
-        return 'Test';
+        $genders = Gender::paginate(10);
+
+        return view('pages.admin.setting.genders.index', compact('genders'));
     }
 }
---
+++ resources/views/pages/admin/setting/genders/index.blade.php
@@ -0,0 +1,77 @@
+@php
+  $inputsAddGender = [
+    [
+      'id'    => 'gender-name',
+      'label' => 'Gender Name',
+      'name'  => 'name'
+    ]
+  ];
+@endphp
+@extends('layouts.app')
+@section('title', 'Genders - Setting')
+@section('content')
+<div class="col-lg-12 mb-4 p-0">
+	<div class="card shadow mb-4">
+		<div class="card-header py-3 d-flex align-items-center">
+			<h6 class="m-0 font-weight-bold text-primary">Gender List</h6>
+			<button type="button" class="btn btn-primary ml-4" data-toggle="modal" data-target="#addGenderModal">Add Gender</button>
+			<form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" method="get"
+				action="">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" name="keyword"
+					value="{{$keyword ?? ''}}" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+		<div class="card-body table-responsive" style="min-height: 400px">
+
+			<x-modal-input 
+				action="{{ route('setting.coins.store') }}"
+				id="addGenderModal"
+				inputs="{!! json_encode($inputsAddGender) !!}"
+				method="POST"
+				title="Add Gender"
+			/>
+
+			<x-modal-input 
+				action="{{ route('setting.coins.update', [1]) }}"
+				id="editGenderModal"
+				inputs="{!! json_encode($inputsAddGender) !!}"
+				method="PUT"
+				title="Edit Gender"
+			/>
+
+			<table class="table table-hover " id="dataTable" width="100%" cellspacing="0">
+				<thead>
+					<tr>
+            <th class="col-1">#</th>
+						<th>Gender</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+					@foreach ($genders as $gender)
+					<tr>
+						<td class="align-middle">{{ $loop->iteration }}</td>
+						<td class="align-middle" style="white-space: nowrap">
+              {{ $gender->name }}
+						</td>
+						<td class="align-middle" style="white-space: nowrap; width: 82px">
+							<button class="btn btn-warning edit-coin" data-gender="{{ $gender }}" data-toggle="modal" data-target="#editGenderModal">
+								<i class="fas fa-edit" onclick=""></i>
+							</button>
+							<form action="{{ route('setting.coins.destroy', [$gender->id]) }}" method="POST" class="d-inline">
+                @method('DELETE') @csrf
+                <button class="btn btn-danger swal-delete" title="Delete"><i class="fas fa-trash"></i></button>
+              </form>
+						</td>
+					</tr>
+					@endforeach
+
+				</tbody>
+			</table>
+			{{ $genders->links() }}
+		</div>
+	</div>
+</div>
+@endsection
