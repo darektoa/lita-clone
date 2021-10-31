e32ae087ce278501c0de3ddd3b4899173dabf584
darektoa <darektoa@gmail.com>
2021-10-31 23:59:53 +0700

    Fixed: Username helper, max username length is 30

--- app/Helpers/UsernameHelper.php
+++ app/Helpers/UsernameHelper.php
@@ -3,19 +3,27 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class UsernameHelper{
   static public function make($string) {
-    $validateToUsername = Validator::make([$string], [
-        0 => 'unique:users,username'
-    ]);
+    $string   = Str::limit($string, 30, '');
+    $rules    = 'unique:users,username';
+    $validate = Validator::make([$string], [0 => $rules]);
+    $unique   = false;
 
-    // $unique = false;
+    if($validate->fails()) $string = Str::limit($string, 20, '');
+    else $unique = true;
 
-    // while(!$unique) {
-      
-    // }
+    while(!$unique) {
+      $newString  = $string . rand(1, 9999999999);
+      $validate   = Validator::make([$newString], [0 => $rules]);
+      if(!$validate->fails()) {
+        $string = $newString;
+        $unique = true;
+      }     
+    }
 
-    return $validateToUsername->fails() ? $string . rand(pow(10, 8 - 1), pow(10, 8) -1) : $string;
+    return $string;
   }
 }
