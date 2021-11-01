561c97b7824b03068b103549951ea755e77b608e
darektoa <darektoa@gmail.com>
2021-11-01 21:30:05 +0700

    Added: B+ tier in pro player skill tier seeder

--- database/seeders/ProPlayerSkillTierSeeder.php
+++ database/seeders/ProPlayerSkillTierSeeder.php
@@ -12,9 +12,10 @@ class ProPlayerSkillTierSeeder extends Seeder
         $tier = new ProPlayerSkillTier();
 
         $tier->create(['name' => 'B',   'price_increase' => 10]);
-        $tier->create(['name' => 'A',   'price_increase' => 20]);
-        $tier->create(['name' => 'A+',  'price_increase' => 30]);
-        $tier->create(['name' => 'S',   'price_increase' => 40]);
-        $tier->create(['name' => 'S+',  'price_increase' => 50]);
+        $tier->create(['name' => 'B+',  'price_increase' => 20]);
+        $tier->create(['name' => 'A',   'price_increase' => 40]);
+        $tier->create(['name' => 'A+',  'price_increase' => 60]);
+        $tier->create(['name' => 'S',   'price_increase' => 90]);
+        $tier->create(['name' => 'S+',  'price_increase' => 120]);
     }
 }
