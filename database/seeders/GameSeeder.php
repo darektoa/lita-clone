6bf628e149ba3ddb6d749968781b47e065884ee2
darektoa <darektoa@gmail.com>
2021-11-01 22:42:07 +0700

    Added: CODM, valorant, LOL, pokemon unite in game seeder

--- database/seeders/GameSeeder.php
+++ database/seeders/GameSeeder.php
@@ -12,19 +12,39 @@ class GameSeeder extends Seeder
         $games = [
             [
                 'name'  => 'Mobile Legends',
-                'icon'  => 'images/game-icons/c2042d83-236c-4e8f-86b9-5af295c11eda.png',
+                'icon'  => 'images/game-icons/2787b04a-b885-41c1-92fa-6134d89d278a.jpg',
                 'tiers' => ['Warrior', 'Elite', 'Master', 'Grand Master', 'Epic', 'Legend', 'Mythic', 'Mythical Glory'],
                 'roles' => ['Assasin', 'Fighter', 'Mage', 'Marksman', 'Support', 'Tank'],
             ], [
                 'name'  => 'Free Fire',
-                'icon'  => 'images/game-icons/ac6256da-0a0d-4ef7-a58e-b5ed59842252.png',
+                'icon'  => 'images/game-icons/7f4bc08c-2ab6-4fb9-90b8-1c5872d19588.jpg',
                 'tiers' => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Heroic'],
                 'roles' => ['Flanker', 'Hitscan', 'Leader', 'Sniper', 'Support', 'Survivor', 'Tracker'],
             ], [
                 'name'  => 'PUBG Mobile',
-                'icon'  => 'images/game-icons/be0b28ea-26c0-43db-92d2-e52df57107ad.png',
+                'icon'  => 'images/game-icons/8dc23b2e-4a50-4b8d-818e-3f37e3f5230f.jpg',
                 'tiers' => ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Crown', 'Ace', 'Conqueror'],
                 'roles' => ['Leader', 'Rusher', 'Scout', 'Sniper', 'Support'],
+            ], [
+                'name'  => 'Call of Duty Mobile',
+                'icon'  => 'images/game-icons/aac7e851-85a4-4727-ab7d-e9516154585c.jpg',
+                'tiers' => ['Rookie', 'Veteran', 'Elite', 'Pro', 'Master', 'Legendary'],
+                'roles' => ['Airborne', 'Clown', 'Defender', 'Mechanic', 'Medic', 'Ninja', 'Scout', 'Trickster'],
+            ], [
+                'name'  => 'Valorant',
+                'icon'  => 'images/game-icons/cbbea757-49b3-42e9-8163-91093a8c6e43.jpg',
+                'tiers' => ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Immortal', 'Radiant'], 
+                'roles' => ['Duelist', 'Controller', 'Initiator', 'Sentinel'],
+            ], [
+                'name'  => 'LOL: Wild Rift',
+                'icon'  => 'images/game-icons/617663f9-3f71-41e9-b95f-0acad5302657.jpg',
+                'tiers' => ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Emerald', 'Diamond', 'Master', 'Grandmaster', 'Challenger'],
+                'roles' => ['Top Laner', 'Jungler', 'Mid Laner', 'Bot Laner', 'Support'],
+            ], [
+                'name'  => 'Pokemon Unite',
+                'icon'  => 'images/game-icons/efed3590-1818-45f2-848f-777599eb9c6a.jpg',
+                'tiers' => ['Beginner', 'Great', 'Expert', 'Veteran', 'Ultra', 'Master'],
+                'roles' => ['Attacker', 'All-Rounder', 'Defender', 'Speedster', 'Supporter'],
             ],
         ];
 
