3b854dbf367fc74d85deef0c2d4266d0e02b525f
darektoa <darektoa@gmail.com>
2021-11-01 09:01:01 +0700

    Fixed: Character validation in username helper

--- app/Helpers/UsernameHelper.php
+++ app/Helpers/UsernameHelper.php
@@ -7,8 +7,9 @@ use Illuminate\Support\Str;
 
 class UsernameHelper{
   static public function make($string) {
-    $string   = Str::limit($string, 30, '');
-    $rules    = 'unique:users,username';
+    $string   = Str::slug(Str::limit($string, 30, ''), '.');
+
+    $rules    = 'regex:/[0-9a-z\._]{5,30}/i|unique:users,username';
     $validate = Validator::make([$string], [0 => $rules]);
     $unique   = false;
 
@@ -17,11 +18,13 @@ class UsernameHelper{
 
     while(!$unique) {
       $newString  = $string . rand(1, 9999999999);
-      $validate   = Validator::make([$newString], [0 => $rules]);
-      if(!$validate->fails()) {
+      $validates   = Validator::make([$newString], [0 => $rules]);
+
+      if(!$validates->fails()){
+        echo false == 0;
         $string = $newString;
         $unique = true;
-      }     
+      }
     }
 
     return $string;
