dd86e97664b0ffc282f0bced345337e5b2153117
darektoa <darektoa@gmail.com>
2021-11-08 22:30:35 +0700

    Added: App banner factory

---
+++ database/factories/AppBannerFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AppBanner;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AppBannerFactory extends Factory
+{
+    protected $model = AppBanner::class;
+
+
+    public function definition()
+    {
+        return [
+            'url'   => 'https://https://source.unsplash.com/335x115/?game',
+            'alt'   => $this->faker->realText(100)
+        ];
+    }
+}
