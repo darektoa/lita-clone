0363723ae1e8abfc955a269c48b6782f91449663
darektoa <darektoa@gmail.com>
2021-11-08 20:16:45 +0700

    Added: App banners migration

---
+++ database/migrations/2021_11_08_131520_create_app_banners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppBannersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('app_banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('alt', 100);
+            $table->text('url');
+            $table->text('link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('app_banners');
+    }
+}
