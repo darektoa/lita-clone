94db87354916afab048250ece113ceadacdeddd0
darektoa <darektoa@gmail.com>
2021-11-11 19:12:20 +0700

    Added: Player relation property in login API response

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
                 ['token' => Hash::make(auth()->user()->id)]
             );
 
-            $user   = User::find(auth()->user()->id);
+            $user   = User::with(['player'])->find(auth()->user()->id);
             $user->token = $loginToken->token;
             return response()->json([
                 'status'    => 200,
