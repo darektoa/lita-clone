548c66c8ab309ceda6ef54919a6705d741422e3a
darektoa <darektoa@gmail.com>
2021-11-15 22:06:22 +0700

    Added: Config FCM package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Kawankoding\Fcm\FcmServiceProvider::class,
 
         /*
          * Application Service Providers...
