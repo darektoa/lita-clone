3d42dbf834973bd145cedff4f55199dac9cba3dd
darektoa <darektoa@gmail.com>
2021-11-16 00:01:38 +0700

    Added: Status name accessor in device id model

--- app/Models/DeviceId.php
+++ app/Models/DeviceId.php
@@ -9,12 +9,25 @@ class DeviceId extends Model
 {
     use HasFactory;
 
-    protected $guarded  = [
-        'id'
-    ];
+    protected $appends  = ['status_name'];
+
+    protected $guarded  = ['id'];
 
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+
+    public function getStatusNameAttribute() {
+        $status = null;
+
+        switch($this->status) {
+            case 0: $status = 'Unsubscibed'; break;
+            case 1: $status = 'Subscribed'; break;
+            default: $status = 'Unknown';
+        }
+
+        return $status;
     }
 }
