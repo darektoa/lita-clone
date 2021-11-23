d1263f6299dcf7364abd8744603afbf4a8ccb0f2
darektoa <darektoa@gmail.com>
2021-11-23 23:39:18 +0700

    Added: FAQs list in admin faws setting page

--- app/Http/Controllers/Web/FAQController.php
+++ app/Http/Controllers/Web/FAQController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\FAQ;
 use Illuminate\Http\Request;
 
 class FAQController extends Controller
 {
     public function index() {
-        return 'Test';
+        $FAQs = FAQ::paginate(10);
+
+        return view('pages.admin.setting.faqs.index', compact('FAQs'));
     }
 }
---
+++ resources/views/pages/admin/setting/faqs/index.blade.php
@@ -0,0 +1,103 @@
+@php
+  $inputsAddFAQ = [
+    [
+      'id'    => 'question',
+      'label' => 'Question',
+      'name'  => 'FAQ-question'
+		], [
+      'id'    => 'answer',
+      'label' => 'Answer',
+      'name'  => 'FAQ-answer'
+    ]
+  ];
+@endphp
+@extends('layouts.app')
+@section('title', 'FAQs - Setting')
+@section('content')
+<div class="col-lg-12 mb-4 p-0">
+	<div class="card shadow mb-4">
+		<div class="card-header py-3 d-flex align-items-center">
+			<h6 class="m-0 font-weight-bold text-primary">FAQ List</h6>
+			<button type="button" class="btn btn-primary ml-4" data-toggle="modal" data-target="#addFAQModal">Add FAQ</button>
+			<form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" method="get"
+				action="">
+				<input class="form-control mr-sm-2" type="search" placeholder="Search" name="keyword"
+					value="{{$keyword ?? ''}}" aria-label="Search">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+			</form>
+		</div>
+		<div class="card-body table-responsive" style="min-height: 400px">
+
+			<x-modal-input 
+				action="{{ '' }}"
+				id="addFAQModal"
+				inputs="{!! json_encode($inputsAddFAQ) !!}"
+				method="POST"
+				title="Add FAQ"
+			/>
+
+			<x-modal-input 
+				action="{{ '' }}"
+				id="editFAQModal"
+				inputs="{!! json_encode($inputsAddFAQ) !!}"
+				method="PUT"
+				title="Edit FAQ"
+			/>
+
+			<table class="table table-hover " id="dataTable" width="100%" cellspacing="0">
+				<thead>
+					<tr>
+            <th class="col-1">#</th>
+						<th>FAQ</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					
+					@foreach ($FAQs as $FAQ)
+					<tr>
+						<td class="align-middle">{{ $loop->iteration }}</td>
+						<td class="align-middle" style="white-space: nowrap">
+              {{ $FAQ->question }}
+						</td>
+						<td class="align-middle" style="white-space: nowrap">
+              {{ $FAQ->answer }}
+						</td>
+						<td class="align-middle" style="white-space: nowrap; width: 82px">
+							<button class="btn btn-warning edit-FAQ" data-FAQ="{{ $FAQ }}" data-toggle="modal" data-target="#editFAQModal">
+								<i class="fas fa-edit" onclick=""></i>
+							</button>
+							<form action="{{ route('setting.FAQs.destroy', [$FAQ->id]) }}" method="POST" class="d-inline">
+                @method('DELETE') @csrf
+                <button class="btn btn-danger swal-delete" title="Delete"><i class="fas fa-trash"></i></button>
+              </form>
+						</td>
+					</tr>
+					@endforeach
+
+				</tbody>
+			</table>
+			{{ $FAQs->links() }}
+		</div>
+	</div>
+</div>
+@endsection
+
+@section('scripts')
+<script>
+  const editFAQButtons = document.querySelectorAll('button.edit-FAQ');
+
+  editFAQButtons.forEach((item) => {
+    item.addEventListener('click', () => {
+      const editForm	  		= document.querySelector('#editFAQModal form');
+      const questionField   = editForm.querySelector('#FAQ-question');
+      const answerField   	= editForm.querySelector('#FAQ-answer');
+      const FAQData 				= JSON.parse(item.dataset.FAQ);
+      const endpoint	  		= `{{ '' }}/${FAQData.id}`;
+      editForm.action   		= endpoint;
+      questionField.value   = FAQData.question;
+      answerField.value   	= FAQData.answer;
+    });
+  });
+</script>
+@endsection
