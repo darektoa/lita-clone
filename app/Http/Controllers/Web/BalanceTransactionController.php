3c284dbb64f249161ac79e3d7c53aa7373e0fdab
darektoa <darektoa@gmail.com>
2021-11-24 23:42:27 +0700

    Added: Balance transaction web controller

---
+++ app/Http/Controllers/Web/BalanceTransactionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BalanceTransactionController extends Controller
+{
+    public function index() {
+        return 'Test';
+    }
+}
