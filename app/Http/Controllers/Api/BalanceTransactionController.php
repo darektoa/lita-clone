83ea8cadcd5b04f482796e9dad457674f5c6f047
darektoa <darektoa@gmail.com>
2021-11-24 23:19:45 +0700

    Added: Withdraw method in balance transaction API controller

--- app/Http/Controllers/Api/BalanceTransactionController.php
+++ app/Http/Controllers/Api/BalanceTransactionController.php
@@ -3,10 +3,41 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\BalanceTransaction;
+use App\Models\{AppSetting, BalanceTransaction};
+use Exception;
 use Illuminate\Http\Request;
 
 class BalanceTransactionController extends Controller
 {
-    //
+    public function withdraw(Request $request) {
+        try{
+            $amount      = $request->amount;
+            $description = $request->description;
+            $user        = auth()->user();
+            $app         = AppSetting::first();
+
+            $transaction = BalanceTransaction::create([
+                'receiver_id'   => $user->id,
+                'balance'       => $amount,
+                'coin'          => round($amount/$app->coin_conversion),
+                'description'   => $description,
+                'type'          => 4,
+                'status'        => 'pending',
+            ]);
+
+            return response()->json([
+                'status'    => 200,
+                'message'   => 'OK',
+                'data'      => $transaction
+            ]);
+
+        }catch(Exception $err) {
+            $errCode    = $err->getCode() ?? 400;
+            $errMessage = $err->getMessage();
+            return response()->json([
+                'status'    => $errCode,
+                'message'   => $errMessage,
+            ]);
+        }
+    }
 }
