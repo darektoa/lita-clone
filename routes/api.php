be12cdfb93d91b33bb297184c8d074b343226db4
darektoa <darektoa@gmail.com>
2021-11-26 23:11:35 +0700

    Added: Pro player order review in API route

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,7 @@ Route::middleware(['auth.api'])->group(function() {
     // ORDER
     Route::prefix('/orders')->group(function() {
         Route::get('/', [ProPlayerOrderController::class, 'index']);
+        Route::post('/{proPlayerOrder:id}/review', [ProPlayerOrderController::class, 'review']);
     });
 
     // PROFILE
