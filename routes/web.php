0a695f5e208107fc552f9c2ef48ee69ccca5bd46
darektoa <darektoa@gmail.com>
2021-11-29 17:56:43 +0700

    Edited: Hide register page in web route

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,8 @@ Route::get('/', fn() => redirect('/login'));
 Route::middleware(['guest'])->group(function() {
   Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginView');
   Route::post('/login', [AuthController::class, 'login'])->name('login');
-  Route::get('/register', [AuthController::class, 'registerView'])->name('register.registerView');
-  Route::post('/register', [AuthController::class, 'register'])->name('register');
+  // Route::get('/register', [AuthController::class, 'registerView'])->name('register.registerView');
+  // Route::post('/register', [AuthController::class, 'register'])->name('register');
 });
 
 Route::middleware(['auth'])->group(function() {
