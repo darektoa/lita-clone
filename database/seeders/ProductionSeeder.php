02a96a79d1352b3f3e0da218756b1a7e55328842
darektoa <darektoa@gmail.com>
2021-11-30 22:58:34 +0700

    Added: Production database seeder

---
+++ database/seeders/ProductionSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ProductionSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+            CoinSeeder::class,
+            TierSeeder::class,
+            GameSeeder::class,
+            GenderSeeder::class,
+            AppBannerSeeder::class, 
+            AppSettingSeeder::class,
+        ]);
+    }
+}
