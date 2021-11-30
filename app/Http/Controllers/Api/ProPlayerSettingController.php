cd6dff6ea608fa020e0884ad0747153d89ffdca3
darektoa <darektoa@gmail.com>
2021-11-30 22:42:46 +0700

    Added: Validation request in activity method pro player setting API controller

--- app/Http/Controllers/Api/ProPlayerSettingController.php
+++ app/Http/Controllers/Api/ProPlayerSettingController.php
@@ -3,20 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\ProPlayerSkill;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProPlayerSettingController extends Controller
 {
     public function activity(Request $request) {
         try{
+            $validator = Validator::make($request->all(), [
+                'activity' => 'required|numeric',
+                'games'    => 'nullable'
+            ]);
+
+            if($validator->fails())
+                return response()->json([
+                    'status'    => 422,
+                    'message'   => 'Unprocessable, Invalid field',
+                    'errors'    => $validator->errors(),
+                ], 422);
+
             $user   = auth()->user();
-            $games  = explode(',', $request->games);
+            $games  = $request->games;
             $skills = $user->player
                 ->proPlayerSkills()
-                ->where('status', 2)
-                ->whereIn('game_id', $games);
+                ->where('status', 2);
+
+            if($games)
+                $skills = $skills->whereIn('game_id', explode(',', $games));
 
             $skills->update([
                 'online'    => $request->activity
