35213c5f6439ee9888637b7816d30376d723b42c
darektoa <darektoa@gmail.com>
2021-12-02 16:01:30 +0700

    Edited: Showing pro player own skills in pro player list API

--- app/Http/Controllers/Api/ProPlayerSkillController.php
+++ app/Http/Controllers/Api/ProPlayerSkillController.php
@@ -40,7 +40,7 @@ class ProPlayerSkillController extends Controller
             $proPlayers = $proPlayers
                 ->where('status', 2) // 2 = Approved
                 //->where('activity', 1) // 1 = Online
-                ->where('player_id', '!=', $playerId)
+                // ->where('player_id', '!=', $playerId)
                 ->inRandomOrder()
                 ->paginate(10);
 
