652b7cbe21826d873924d70b6da9380a84a31d72
darektoa <darektoa@gmail.com>
2021-12-02 23:58:31 +0700

    Added: Send custom notification method in notification API controller

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\DeviceId;
+use App\Models\{DeviceId, User};
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class NotificationController extends Controller
 {
@@ -53,6 +54,50 @@ class NotificationController extends Controller
             return response()->json([
                 'status'    => $errCode,
                 'message'   => $errMessage
+            ], $errCode);
+        }
+    }
+
+
+    public function send(Request $request) {
+        try{
+            $user   = User::find($request->user_id);
+
+            if(!$user)
+                throw new Exception('Unprocessable, User not found', 422);
+
+            $recipient = Arr::flatten([
+                $user
+                ->deviceIds()
+                ->select('device_id')
+                ->get()
+                ->makeHidden('status_name')
+                ->toArray()
+            ]);
+
+            $payloads = [
+                'title' => $request->title,
+                'body'  => $request->body,
+            ];
+
+            fcm()->to($recipient)
+            ->timeToLive(86400) // 1 day
+            ->data($payloads)
+            ->notification($payloads)
+            ->send();
+
+            return response()->json([
+                'status'    => 200,
+                'message'   => 'OK',
+                'data'      => $request->all(),
+            ]);
+        }catch(Exception $err) {
+            $errCode    = $err->getCode() ? 400 : 400;
+            $errMessage = $err->getMessage();
+
+            return response()->json([
+                'status'    => $errCode,
+                'message'   => $errMessage,
             ], $errCode);
         }
     }
