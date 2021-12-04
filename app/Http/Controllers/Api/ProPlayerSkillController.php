fe17b4c5fe5328df050d35e7ee7a28bd4594335a
darektoa <darektoa@gmail.com>
2021-12-04 17:16:44 +0700

    Added: User relationship in reviews method pro player skill API controller

--- app/Http/Controllers/Api/ProPlayerSkillController.php
+++ app/Http/Controllers/Api/ProPlayerSkillController.php
@@ -429,6 +429,7 @@ class ProPlayerSkillController extends Controller
     public function reviews(ProPlayerSkill $proPlayerSkill) {
         $reviews = $proPlayerSkill
             ->proPlayerOrderReviews()
+            ->with('proPlayerOrder.proPlayerSkill.player.user')
             ->paginate(10);
 
         return response()->json(
