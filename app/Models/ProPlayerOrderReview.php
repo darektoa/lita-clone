6f367f677c782790df79ca0ee61f9281cc7b7c98
darektoa <darektoa@gmail.com>
2021-12-04 17:00:33 +0700

    Added: User relationship method in pro player order review model

--- app/Models/ProPlayerOrderReview.php
+++ app/Models/ProPlayerOrderReview.php
@@ -15,4 +15,15 @@ class ProPlayerOrderReview extends Model
     public function proPlayerOrder() {
         return $this->belongsTo(ProPlayerOrder::class);
     }
+
+
+    public function user() {
+        $relation = $this->belongsTo(ProPlayerOrder::class, 'pro_player_order_id');
+
+        return $relation
+            ->select('users.*')
+            ->leftJoin('pro_player_skills', 'pro_player_orders.pro_player_skill_id', '=', 'pro_player_skills.id')
+            ->leftJoin('players', 'pro_player_skills.player_id', '=', 'players.id')
+            ->leftJoin('users', 'players.user_id', '=', 'users.id');
+    }
 }
