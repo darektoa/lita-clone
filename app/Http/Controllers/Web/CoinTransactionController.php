b3adc75dae50f6c2f2e457c042a48024c6e04d04
darektoa <darektoa@gmail.com>
2021-12-08 00:44:37 +0700

    Added: Coin transaction web controller

---
+++ app/Http/Controllers/Web/CoinTransactionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\CoinTransaction;
+use Illuminate\Http\Request;
+
+class CoinTransactionController extends Controller
+{
+    public function index() {
+        return 'Test';
+    }
+}
