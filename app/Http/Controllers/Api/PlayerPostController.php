06ad784acfab94bfcfe32ef75dbca4e859068aed
darektoa <darektoa@gmail.com>
2021-12-08 23:23:15 +0700

    Added: Requests validation in store method of player post API controller

--- app/Http/Controllers/Api/PlayerPostController.php
+++ app/Http/Controllers/Api/PlayerPostController.php
@@ -6,11 +6,23 @@ use App\Http\Controllers\Controller;
 use App\Models\{PlayerPost, PlayerPostMedia, User};
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PlayerPostController extends Controller
 {
     public function store(Request $request) {
         try{
+            $validator  = Validator::make($request->all(), [
+                'text'  => 'nullable|max:4000'
+            ]);
+
+            if($validator->fails())
+                return response()->json([
+                    'status'    => 422,
+                    'message'   => 'Unprocessable, Invalid field',
+                    'errors'    => $validator->errors()->all(),
+                ]);
+
             $userId = auth()->user()->id;
             $user   = User::with(['player'])->find($userId);
 
