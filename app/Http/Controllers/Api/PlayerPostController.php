6527224f8b879c3a77d4d6d92c26374ced2465be
darektoa <darektoa@gmail.com>
2021-12-09 00:25:00 +0700

    Added: Media field in player post API route

--- app/Http/Controllers/Api/PlayerPostController.php
+++ app/Http/Controllers/Api/PlayerPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\StorageHelper;
 use App\Http\Controllers\Controller;
 use App\Models\{PlayerPost, PlayerPostMedia, User};
 use Exception;
@@ -13,7 +14,9 @@ class PlayerPostController extends Controller
     public function store(Request $request) {
         try{
             $validator  = Validator::make($request->all(), [
-                'text'  => 'nullable|max:4000'
+                'text'      => 'nullable|max:4000',
+                'media'     => 'nullable|array|max:20',
+                'media.*'   => 'mimes:jpeg,jpg,png,bmp,gif,svg,mp4,avi,mpeg,qt|max:30720' // MAX:30MB
             ]);
 
             if($validator->fails())
@@ -25,11 +28,28 @@ class PlayerPostController extends Controller
 
             $userId = auth()->user()->id;
             $user   = User::with(['player'])->find($userId);
+            $text   = $request->text;
+            $media  = $request->media;                            
+
+            if(!$text && !$media)
+                throw new Exception('Unprocessable, One of the text or media fields is required', 422);
+            if(!$user->player->is_pro_player)
+                throw new Exception('Unprocessable, Only pro players can post', 422);
 
             $post = PlayerPost::create([
                 'player_id' => $user->player->id,
-                'text'  => $request->text
+                'text'  => $text
             ]);
+
+            if($media) foreach($media as $item) {
+                $mediaPath  = StorageHelper::put('images/users/posts', $item);
+                PlayerPostMedia::create([
+                    'player_post_id'    => $post->id,
+                    'url'               => $mediaPath,
+                ]);
+            }
+
+            $post   = $post->load('postMedia');
 
             return response()->json([
                 'status'    => 200,
