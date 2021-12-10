4b672ec16fd526cc4bd2bed60bd187b7fa4ca61c
darektoa <darektoa@gmail.com>
2021-12-10 23:32:19 +0700

    Edited: Search button color to primary color in pro player skill request list admin page

--- resources/views/pages/admin/pro-players/index.blade.php
+++ resources/views/pages/admin/pro-players/index.blade.php
@@ -16,7 +16,7 @@
 				action="">
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" name="keyword"
 					value="{{$keyword ?? ''}}" aria-label="Search">
-				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				<button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Search</button>
 			</form>
 		</div>
 		<div class="card-body table-responsive" style="min-height: 400px">
