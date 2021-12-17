c05c7ed7f2b9c425474f2642a2d0caad65e7d362
darektoa <darektoa@gmail.com>
2021-12-17 17:09:24 +0700

    Added: Terms method in app info API controller

--- app/Http/Controllers/Api/AppInfoController.php
+++ app/Http/Controllers/Api/AppInfoController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\AppSetting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AppInfoController extends Controller
 {
-    //
+    public function terms() {
+        return response()->json([
+            'status'    => 200,
+            'message'   => 'OK',
+            'data'      => AppSetting::first()->terms_rules,
+        ]);
+    }
 }
