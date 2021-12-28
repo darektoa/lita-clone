8ba1c7471499ad2927b21fff359cf2e2f6d5c28f
darektoa <darektoa@gmail.com>
2021-12-28 17:23:03 +0700

    Added: Index method for list available transfers in available transfer API controller

--- app/Http/Controllers/Api/AvailableTransferController.php
+++ app/Http/Controllers/Api/AvailableTransferController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
+use App\Models\AvailableTransfer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class AvailableTransferController extends Controller
 {
-    //
+    public function index() {
+        $availables = AvailableTransfer::all();
+
+        return ResponseHelper::make($availables);
+    }
 }
