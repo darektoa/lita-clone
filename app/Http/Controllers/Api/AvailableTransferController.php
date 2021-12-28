e74d34b08d30be716f07d7d58ebb7cd1d83ca8ea
darektoa <darektoa@gmail.com>
2021-12-28 17:28:55 +0700

    Added: Using available transfer API resource in index method of available transfer API controller

--- app/Http/Controllers/Api/AvailableTransferController.php
+++ app/Http/Controllers/Api/AvailableTransferController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Helpers\ResponseHelper;
 use App\Models\AvailableTransfer;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\AvailableTransferResource;
 use Illuminate\Http\Request;
 
 class AvailableTransferController extends Controller
@@ -12,6 +13,6 @@ class AvailableTransferController extends Controller
     public function index() {
         $availables = AvailableTransfer::all();
 
-        return ResponseHelper::make($availables);
+        return ResponseHelper::make(AvailableTransferResource::collection($availables));
     }
 }
