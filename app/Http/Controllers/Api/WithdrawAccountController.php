8107bb016b70015549ba201c8fb1984fe14b9dda
darektoa <darektoa@gmail.com>
2021-12-28 20:28:33 +0700

    Added: Store method for add withdraw account in withdraw account API controller

--- app/Http/Controllers/Api/WithdrawAccountController.php
+++ app/Http/Controllers/Api/WithdrawAccountController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Models\WithdrawAccount;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class WithdrawAccountController extends Controller
 {
@@ -15,5 +17,38 @@ class WithdrawAccountController extends Controller
             ->paginate(10);
 
         return ResponseHelper::paginate($accounts);
+    }
+
+
+    public function store(Request $request) {
+        try{
+            $validator  = Validator::make($request->all(), [
+                'name'          => 'required|max:100',
+                'number'        => 'required|max:20',
+                'transfer_id'   => 'required|exists:available_transfers,id'
+            ]);
+
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable, Invalid field', 422, $errors);
+            }
+
+            $user     = auth()->user();
+            $account  = WithdrawAccount::create([
+                'user_id'       => $user->id,
+                'name'          => $request->name,
+                'number'        => $request->number,
+                'transfer_id'   => $request->transfer_id,
+                'default'       => $user->withdrawAccounts->count() ? 0 : 1,
+            ]);
+
+            return ResponseHelper::make($account);
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
     }
 }
