c56c78031eedd07977aad3f9caa6f0b0527d04b8
darektoa <darektoa@gmail.com>
2021-12-28 19:11:27 +0700

    Added: Index method for withdraw account list in withdraw account API controller

--- app/Http/Controllers/Api/WithdrawAccountController.php
+++ app/Http/Controllers/Api/WithdrawAccountController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
+use App\Models\WithdrawAccount;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class WithdrawAccountController extends Controller
 {
-    //
+    public function index() {
+        $user       = auth()->user();
+        $accounts   = WithdrawAccount::whereRelation('user', 'id', '=', $user->id)
+            ->paginate(10);
+
+        return ResponseHelper::paginate($accounts);
+    }
 }
