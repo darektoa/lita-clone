20469dee41d0978b14df2a64d2834ba7079b0585
darektoa <darektoa@gmail.com>
2022-01-01 11:38:21 +0700

    Deleted: History method in coin API controller

--- app/Http/Controllers/Api/CoinController.php
+++ app/Http/Controllers/Api/CoinController.php
@@ -11,23 +11,7 @@ class CoinController extends Controller
 {
     public function index() {
         $coins = Coin::all();
-        
+
         return ResponseHelper::make($coins);
-    }
-
-
-    public function history(Request $request) {
-        $user = auth()->user();
-        $purchases = CoinPurchase::withTrashed();
-        $statusId = (int)$request->status;
-
-        if($statusId >= 0 && $statusId < 5) $purchases = $purchases->where('status', $statusId);
-        if($user->admin) $purchases = $purchases->latest();
-        else $purchases = $purchases->where('player_id', $user->player->id);
-
-        $purchases = $purchases
-            ->paginate(10);
-        
-        return response()->json(['data' => $purchases]);
     }
 }
