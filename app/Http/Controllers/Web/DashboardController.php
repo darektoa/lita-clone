6e2059009e18cec2d5ac3adeaa5b039451c87819
darektoa <darektoa@gmail.com>
2022-01-02 17:13:12 +0700

    Added: Admin method for handle admin dashboard in dashboard web controller

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Models\CoinPurchase;
+use App\Models\{
+    BalanceTransaction,CoinTransaction, Player, ProPlayerOrder, User};
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,16 +12,37 @@ class DashboardController extends Controller
     public function __invoke(Request $request)
     {
         $user = auth()->user();
-        $total = collect([]);
-        $coinPurchase = null;
 
-        if($user->admin) $coinPurchase = new CoinPurchase;
-        else $coinPurchase = CoinPurchase::where('player_id', $user->player->id);
+        if($user->admin) return $this->admin($request);
+        // if($user->player) return $this->player($request);
+    }
+
+
+    public function admin(Request $request) {
+        $balanceTransaction = new BalanceTransaction();
+        $coinTransaction    = new CoinTransaction();
+        $proPlayerOrder     = new ProPlayerOrder();
+        $user               = new User();
+
+        $total = json_decode(collect([
+            'balanceTransaction' => [
+                'all'       => $balanceTransaction->count(),
+                'withdraw'  => $balanceTransaction->where('type', 4)->count(),
+            ],
+            'coinTransaction' => [
+                'all'       => $coinTransaction->count(),
+                'paid'      => $coinTransaction->where('status', 'paid')->count(),
+                'topup'     => $coinTransaction->where('type', 0)->count(),
+            ],
+            'proPlayerOrder' => [
+                'all'       => $proPlayerOrder->count(),
+            ],
+            'user' => [
+                'all'       => $user->count(),
+                'player'    => $user->has('player')->count(),
+            ]
+        ]));
         
-        $total->put('approved', $coinPurchase->where('status', 2)->count());
-        $total->put('pending',  $coinPurchase->where('status', 0)->count());
-        $total->put('rejected', $coinPurchase->where('status', 1)->count());
-
         return view('pages.general.dashboard', compact('total'));
     }
 }
