4ad4f1367c5efb3abca1df7391030eb58a5d7cee
darektoa <darektoa@gmail.com>
2022-01-02 17:17:32 +0700

    Added: Adjust info box value, color in player dashboard view

--- resources/views/pages/user/dashboard.blade.php
+++ resources/views/pages/user/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
-  <x-dashboard.info-box title="My Coins" value="{{ auth()->user()->player->coin }}" color="info" icon="fa-coins text-warning"/>
-  <x-dashboard.info-box title="Topup Success" value="{{ $total['approved'] }}" color="success" icon="fa-check text-gray-300"/>
-  <x-dashboard.info-box title="Topup Pending" value="{{ $total['pending'] }}" color="warning" icon="fa-clock text-gray-300"/>
-  <x-dashboard.info-box title="Topup Failed" value="{{ $total['rejected'] }}" color="danger" icon="fa-ban text-gray-300"/>
+  <x-dashboard.info-box title="My Coins" value="{{ $total->coins->all }}" color="primary" icon="fa-coins text-primary"/>
+  <x-dashboard.info-box title="Topup Success" value="{{ $total->coinReceivingTransaction->paid }}" color="success" icon="fa-check text-gray-300"/>
+  <x-dashboard.info-box title="Topup Pending" value="{{ $total->coinReceivingTransaction->pending }}" color="warning" icon="fa-clock text-gray-300"/>
+  <x-dashboard.info-box title="Topup Expired" value="{{ $total->coinReceivingTransaction->expired }}" color="danger" icon="fa-ban text-gray-300"/>
 </div>
