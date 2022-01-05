eb33f0eb8e7e4240308e6f9cdd66e25ef87e792b
darektoa <darektoa@gmail.com>
2022-01-05 22:58:55 +0700

    Added: Using chart trait in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\ChartTrait;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -11,7 +12,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use ChartTrait, HasApiTokens, HasFactory, Notifiable, SoftDeletes;
     
     protected $casts    = [
         'email_verified_at' => 'datetime',
