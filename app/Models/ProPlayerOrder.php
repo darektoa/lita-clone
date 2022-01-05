b3c32457643f017cf888756483d1306c51d54c72
darektoa <darektoa@gmail.com>
2022-01-05 23:08:17 +0700

    Added: Using chart trait in pro player order model

--- app/Models/ProPlayerOrder.php
+++ app/Models/ProPlayerOrder.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\ChartTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProPlayerOrder extends Model
 {
-    use HasFactory;
+    use ChartTrait, HasFactory;
 
     protected $guarded  = ['id'];
 
