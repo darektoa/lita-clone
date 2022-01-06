f299e8d9c4eae34369aa0b7617fe2cc3f3e83691
darektoa <darektoa@gmail.com>
2022-01-06 22:04:15 +0700

    Added: Eficiency get data from model in 'chart by created at' method of chart trait

--- app/Traits/ChartTrait.php
+++ app/Traits/ChartTrait.php
@@ -8,12 +8,17 @@ use Carbon\Carbon;
 trait ChartTrait{
     static public function chartByCreatedAt($startDate, $endDate) {
         $labels = DateHelper::range($startDate, $endDate);
-        $data   = collect($labels)->map(function($item) {
-            $date   = Carbon::createFromFormat('d M y', $item)->toDateString();
-            $total  = self::whereDate('created_at', $date)->count();
-            return $total;
-        });
+        $model  = self::whereBetween('created_at', [$startDate, $endDate])
+            ->selectRaw('DATE(created_at) as date, COUNT(*) as total')
+            ->groupByRaw('date')
+            ->get();
 
+        $data   = collect($labels)->map(function($item) use($model) {
+            $date = Carbon::createFromFormat('d M y', $item)->toDateString();
+            $data = $model->where('date', $date)->first();
+            return $data->total ?? 0;
+        });
+        
         return [
             'labels' => $labels,
             'data'   => $data,
