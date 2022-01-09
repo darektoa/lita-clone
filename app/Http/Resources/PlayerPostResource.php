36926c88feca0887b151a0b52eb6768ebd1d2fa4
darektoa <darektoa@gmail.com>
2022-01-09 20:47:38 +0700

    Added: User property in player post API resource

--- app/Http/Resources/PlayerPostResource.php
+++ app/Http/Resources/PlayerPostResource.php
@@ -8,12 +8,16 @@ class PlayerPostResource extends JsonResource
 {
     public function toArray($request)
     {
+        $playerLoaded   = $this->relationLoaded('player');
+        $userLoaded     = $playerLoaded ? $this->player->relationLoaded('user') : false;
+
         return [
             'id'            => $this->id,
             'text'          => $this->text,
             'created_at'    => $this->created_at,
             'updated_at'    => $this->updated_at,
             'post_media'    => PlayerPostMediaResource::collection($this->whenLoaded('postMedia')),
+            'user'          => UserResource::make($this->when($userLoaded, $this->player->user)),
         ];
     }
 }
