9f09eeffd5083cab1f7527f5f1fd82c54047e7bc
darektoa <darektoa@gmail.com>
2022-01-11 21:54:38 +0700

    Added: Follower list API route by username

--- routes/api.php
+++ routes/api.php
@@ -146,6 +146,7 @@ Route::middleware(['auth.api'])->group(function() {
 
         Route::get('/follow', [ProPlayerController::class, 'follow']);
         Route::get('/unfollow', [ProPlayerController::class, 'unfollow']);
+        Route::get('/followers', [ProPlayerController::class, 'followers']);
     });
 });
 
