90934861e43485756dbe174799b20f40a382c593
darektoa <darektoa@gmail.com>
2022-01-12 15:08:09 +0700

    Added: Show method in player API controller

--- app/Http/Controllers/Api/PlayerController.php
+++ app/Http/Controllers/Api/PlayerController.php
@@ -2,10 +2,17 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\User;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-    //
+    public function show(User $user) {
+        return ResponseHelper::make(
+            UserResource::make($user->load('player'))
+        );
+    }
 }
