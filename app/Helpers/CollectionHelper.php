8736546af941b7f9d030acfa01046388ace3a823
darektoa <darektoa@gmail.com>
2022-01-16 05:31:04 +0700

    Added: Paginate method in collection helper

--- app/Helpers/CollectionHelper.php
+++ app/Helpers/CollectionHelper.php
@@ -2,6 +2,27 @@
 
 namespace App\Helpers;
 
+use Illuminate\Pagination\{LengthAwarePaginator, Paginator};
+
 class CollectionHelper{
-    
+    static public function paginate(
+        $items,
+        int $perPage=15,
+        string $pageName='page',
+        int $page=null
+    ) {
+        $page       = $page ?: Paginator::resolveCurrentPage();
+        $options    = [
+            'path'      => Paginator::resolveCurrentPath(),
+            'pageName'  => $pageName,
+        ];
+        
+        return new LengthAwarePaginator(
+            $items,
+            $items->count(),
+            $perPage,
+            $page,
+            $options,
+        );
+    }
 }
