2b5c1108e59f16c9f112d667aadbe8eb32d60ca8
darektoa <darektoa@gmail.com>
2022-01-15 07:34:16 +0700

    Added: Sort transactions by latest in index method of coin transaction web controller

--- app/Http/Controllers/Web/CoinTransactionController.php
+++ app/Http/Controllers/Web/CoinTransactionController.php
@@ -9,8 +9,8 @@ use Illuminate\Http\Request;
 class CoinTransactionController extends Controller
 {
     public function index() {
-        $transactions = new CoinTransaction();
-        $transactions = $transactions->paginate(10);
+        $transactions = CoinTransaction::latest()
+            ->paginate(10);
 
         return view('pages.general.coin.index', compact('transactions'));
     }
