40edc2d1e755d04b92195dec49068c439ed4a008
darektoa <darektoa@gmail.com>
2022-01-20 05:33:15 +0700

    Edited: Copyright name in footer to yukita

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
   <div class="container my-auto">
       <div class="copyright text-center my-auto">
-          <span>Copyright &copy; Lita Clone 2021</span>
+          <span>Copyright &copy; Yukita 2021</span>
       </div>
   </div>
 </footer>
