a39f47c6cafd295924838b731ac7a798529e77da
darektoa <darektoa@gmail.com>
2022-01-23 05:15:32 +0700

    Added: Index method in pro player order web controller

--- app/Http/Controllers/Web/ProPlayerOrderController.php
+++ app/Http/Controllers/Web/ProPlayerOrderController.php
@@ -3,9 +3,21 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProPlayerOrder;
 use Illuminate\Http\Request;
 
 class ProPlayerOrderController extends Controller
 {
-    
+    public function index(Request $request) {
+        $search     = $request->search;
+        $statusId   = $request->satatus;
+        $orders     = new ProPlayerOrder;
+        
+        $orders = $orders
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
+
+        return view('pages.admin.orders.index', compact('orders'));
+    }
 }
