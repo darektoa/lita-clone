df87bcd36debc3b986b01b940a8b901027325ec2
darektoa <darektoa@gmail.com>
2022-01-22 08:19:23 +0700

    Added: Search request in coin transaction web controller

--- app/Http/Controllers/Web/CoinTransactionController.php
+++ app/Http/Controllers/Web/CoinTransactionController.php
@@ -9,15 +9,25 @@ use Illuminate\Http\Request;
 class CoinTransactionController extends Controller
 {
     public function index(Request $request) {
+        $search         = $request->search;
         $typeId         = $request->type;
         $transactions   = new CoinTransaction;
 
         if($typeId !== null & $typeId >= 0 && $typeId <= 2)
             $transactions = $transactions->where('type', $typeId);
+        if($search)
+            $transactions  = $transactions
+            ->whereHas('receiver', function($query) use($search) {
+                $query
+                ->where('username', 'LIKE', "%$search%")
+                ->orWhere('email', 'LIKE', "%$search%")
+                ->orWhere('name', 'LIKE', "%$search%");
+            });
 
         $transactions = $transactions
             ->latest()
-            ->paginate(10);
+            ->paginate(10)
+            ->withQueryString();
 
         return view('pages.general.coin.index', compact('transactions'));
     }
