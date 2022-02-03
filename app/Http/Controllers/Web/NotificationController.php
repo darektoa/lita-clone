d5896b2ae3bde189d32499dc6e8a63910868b223
darektoa <darektoa@gmail.com>
2022-02-04 05:03:59 +0700

    Add: Index method in notification web controller

--- app/Http/Controllers/Web/NotificationController.php
+++ app/Http/Controllers/Web/NotificationController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Notification;
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    //
+    public function index() {
+        $userId         = auth()->id();
+        $notifications  = Notification::whereHas('notifiable', function($query) use($userId) {
+            $query->whereHas('admin')
+                ->where('id', $userId);
+        })->paginate(10);
+
+        return view('pages.admin.notifications.index', compact('notifications'));
+    }
 }
