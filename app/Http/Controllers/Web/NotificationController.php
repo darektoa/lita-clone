4a0a0a518d1a4570ec77ae3c16cde44a78cd1c00
darektoa <darektoa@gmail.com>
2022-02-04 05:06:38 +0700

    Add: Massive method in notification web controller

--- app/Http/Controllers/Web/NotificationController.php
+++ app/Http/Controllers/Web/NotificationController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
-use App\Models\Notification;
+use App\Models\{Notification, User};
+use App\Notifications\PushNotification;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\{Notification as Notif};
+use RealRashid\SweetAlert\Facades\Alert;
 
 class NotificationController extends Controller
 {
@@ -16,5 +20,41 @@ class NotificationController extends Controller
         })->paginate(10);
 
         return view('pages.admin.notifications.index', compact('notifications'));
+    }
+
+
+    public function massive(Request $request) {
+        try{
+            $recipient  = (int) $request->recipient;
+            $users      = new User();
+
+            switch($recipient) {
+                case 1:
+                    $users      = $users->whereHas('player');
+                    $recipient  = ['Player', 'Pro Player']; break;
+                case 2:
+                    $users = $users->whereRelation('player', 'is_pro_player', 0);
+                    $recipient  = ['Player']; break;
+                case 3:
+                    $users = $users->whereRelation('player', 'is_pro_player', 1);
+                    $recipient  = ['Pro Player']; break;
+            }
+            
+            $users      = $users->orWhereHas('admin')->get();
+            $payloads   = [
+                'title'     => $request->title,
+                'body'      => $request->body,
+                'recipient' => $recipient,
+                'admin_id'  => auth()->id(),
+            ];
+
+            Notif::send($users, new PushNotification($payloads));
+            Alert::success('Success', 'Sent notifications successfully');
+        }catch(Exception $err) {
+            $errMessage = $err->getMessage();
+            Alert::error('Failed', $errMessage);
+        }finally{
+            return back();
+        }
     }
 }
