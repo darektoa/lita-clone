3b7b5a70178a291289d203dedae29ff31646d288
darektoa <darektoa@gmail.com>
2022-02-07 05:16:37 +0700

    Add: Send coin view

---
+++ resources/views/pages/admin/coin/send.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+@section('title', 'Send Coin')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 mb-4">
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold text-primary">Send Coin</h6>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    <form action="" method="post" class="col-lg-6" enctype="multipart/form-data">
+                        @csrf
+    
+                        <div class="form-group">
+                            <label for="Jumlah Cuti">Player ID <span class="text-danger">*</span></label>
+                            <input id="player_id" name="player_id" class="form-control" type="text" required>
+                        </div>
+    
+                        <div class="form-group">
+                            <label for="Jumlah Cuti">Coin <span class="text-danger">*</span></label>
+                            <input id="coin" name="coin" class="form-control" type="number" min="1" required>
+                        </div>
+    
+                        <button type="submit" class="btn btn-primary">Send</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+{{-- @section('scripts')
+    <script>
+        const coins = @json($coins);
+    </script>
+    <script src="{{ asset('js/scripts/pages/user/coin/topup.js') }}" type="module"></script>
+@endsection --}}
