eeb40090c781b6aabc6d841db310b801e37e19bf
darektoa <darektoa@gmail.com>
2022-02-13 05:12:00 +0700

    Add: Using ended pro player order total in total order card info dashboard admin page

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
   <x-dashboard.info-box title="Total Players" value="{{ $total->user->player }}" color="primary" icon="fa-users text-primary"/>
-  <x-dashboard.info-box title="Total Order" value="{{ $total->proPlayerOrder->all }}" color="info" icon="fa-shopping-cart text-gray-300"/>
+  <x-dashboard.info-box title="Total Order" value="{{ $total->proPlayerOrder->ended }}" color="info" icon="fa-shopping-cart text-gray-300"/>
   <x-dashboard.info-box title="Total Topup" value="{{ $total->coinTransaction->paid }}" color="success" icon="fa-coins text-gray-300"/>
   <x-dashboard.info-box title="Total Withdraw" value="{{ $total->balanceTransaction->withdraw }}" color="warning" icon="fa-money-bill-wave-alt text-gray-300"/>
 </div>
