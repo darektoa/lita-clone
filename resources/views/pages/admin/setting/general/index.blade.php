7aab1cbbcaa39049f4ab9e1713ff52a8ec094acc
darektoa <darektoa@gmail.com>
2022-02-17 05:05:37 +0700

    Edit: Submit button color to primary color in general setting admin page

--- resources/views/pages/admin/setting/general/index.blade.php
+++ resources/views/pages/admin/setting/general/index.blade.php
@@ -29,7 +29,7 @@
           </div>
 
           <div class="form-group">
-            <button type="submit" class="btn btn-success">Save</button>
+            <button type="submit" class="btn btn-primary">Save</button>
           </div>
         </form>
       </div>
