4d25640ad9511fa1d23e5288be58005744ed490e
darektoa <darektoa@gmail.com>
2022-02-24 05:08:56 +0700

    Add: Using Chat media resource in store method of chat media API controller

--- app/Http/Controllers/Api/ChatMediaController.php
+++ app/Http/Controllers/Api/ChatMediaController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Exceptions\ErrorException;
 use App\Helpers\{ResponseHelper, StorageHelper};
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ChatMediaResource;
 use App\Models\ChatMedia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -32,7 +33,9 @@ class ChatMediaController extends Controller
                 'alt'           => $request->alt,
             ]);
 
-            return ResponseHelper::make($media);
+            return ResponseHelper::make(
+                ChatMediaResource::make($media)
+            );
         }catch(ErrorException $err) {
             return ResponseHelper::error(
                 $err->getErrors(),
