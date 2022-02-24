3c392d843ebb5937fe39ae4740650f87c85fe595
darektoa <darektoa@gmail.com>
2022-02-25 05:04:55 +0700

    Edit: Incrementing property to false in chat media model

--- app/Models/ChatMedia.php
+++ app/Models/ChatMedia.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class ChatMedia extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public $incrementing = false;
 
 
     public function sender() {
