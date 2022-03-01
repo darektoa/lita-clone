904a27bd891c0572334498fd8fcb3feab548167b
darektoa <darektoa@gmail.com>
2022-03-02 05:07:25 +0700

    Add: Store method in daily coin API controller

--- app/Http/Controllers/Api/DailyCoinController.php
+++ app/Http/Controllers/Api/DailyCoinController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Models\DailyCoin;
 use Illuminate\Http\Request;
 
 class DailyCoinController extends Controller
 {
-    //
+    public function store() {
+        try{
+            $user       = auth()->user();
+            $claimed    = DailyCoin::claim($user);
+
+            return ResponseHelper::make($claimed);
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
 }
