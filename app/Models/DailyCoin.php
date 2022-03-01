a7c6931447d622fa190d1ce49c273c34e973fc32
darektoa <darektoa@gmail.com>
2022-03-02 05:02:18 +0700

    Add: Generate data method in daily coin model

--- app/Models/DailyCoin.php
+++ app/Models/DailyCoin.php
@@ -19,4 +19,24 @@ class DailyCoin extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+
+    protected function generateData($quantity=10) {
+        $coin           = 10;
+        $data           = collect([]);
+        $tz             = +7;
+        $coinConversion = AppSetting::first()->coin_conversion;
+
+        for($i = 0; $i < $quantity; $i++) {
+            $data->push([
+                'id'            => $i + 1,
+                'coin'          => $coin,
+                'balance'       => $coin * $coinConversion,
+                'claimed_at'    => null,
+                'created_at'    => now()->addHours($tz)->addDays($i)->toDateString(),
+            ]);
+        }
+
+        return $data;
+    }
 }
