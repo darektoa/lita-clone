a82ce0d68f06f36d62334ec2f1649595cc917888
darektoa <darektoa@gmail.com>
2022-03-05 05:04:12 +0700

    Add: Banned status name in get status name attribute method of pro player skill model

--- app/Models/ProPlayerSkill.php
+++ app/Models/ProPlayerSkill.php
@@ -74,6 +74,7 @@ class ProPlayerSkill extends Model
             case 0: $statusName = 'Pending'; break;
             case 1: $statusName = 'Rejected'; break;
             case 2: $statusName = 'Approved'; break;
+            case 3: $statusName = 'Banned'; break;
             default: $statusName = 'Unknown';
         }
 
