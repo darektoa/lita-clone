92d15bf47720941ed902c9e7045864a0058ed63b
darektoa <darektoa@gmail.com>
2022-03-11 05:02:12 +0700

    Add: Pro player services table migration

---
+++ database/migrations/2022_02_09_115945_create_pro_player_services_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProPlayerServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pro_player_services', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('player_id')->constrained();
+            $table->foreignId('service_id')->constrained();
+            $table->string('bio')->nullable();
+            $table->float('rate', 3, 2)->default(0);
+            $table->smallInteger('status');
+            $table->smallInteger('activity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pro_player_services');
+    }
+}
