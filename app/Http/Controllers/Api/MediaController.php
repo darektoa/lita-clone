93448d224bff91e34e73fa7f9feb49b87eb0f94f
darektoa <darektoa@gmail.com>
2022-03-13 05:02:10 +0700

    Add: Store method in media API controller

--- app/Http/Controllers/Api/MediaController.php
+++ app/Http/Controllers/Api/MediaController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Exceptions\ErrorException;
+use App\Helpers\ResponseHelper;
+use App\Helpers\StorageHelper;
 use Illuminate\Http\Request;
 
 class MediaController extends Controller
 {
-    //
+    public function store(Request $request) {
+        try{
+            $media  = $request->upload;
+            $path   = StorageHelper::put('media/other', $media);
+            $url    = StorageHelper::url($path);
+                
+            return response()->json([
+                'uploaded'  => true,
+                'url'       => $url,
+            ], 200);
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
+    }
 }
