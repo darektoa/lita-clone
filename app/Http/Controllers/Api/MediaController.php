dab4ed0cd842768cd6089ada70d269146340eebf
darektoa <darektoa@gmail.com>
2022-03-13 05:03:27 +0700

    Add: User validation with token in store method of media API controller

--- app/Http/Controllers/Api/MediaController.php
+++ app/Http/Controllers/Api/MediaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\LoginToken;
 use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Helpers\StorageHelper;
@@ -13,6 +14,15 @@ class MediaController extends Controller
     public function store(Request $request) {
         try{
             $media  = $request->upload;
+            $token  = $request->token;
+            $token  = LoginToken::with('user.admin')
+                ->whereToken($token)
+                ->whereHas('user', function($user) {
+                    $user->whereHas('admin');
+                })->first();
+
+            if(!$token) throw new ErrorException('Unauthorized', 401);
+
             $path   = StorageHelper::put('media/other', $media);
             $url    = StorageHelper::url($path);
                 
