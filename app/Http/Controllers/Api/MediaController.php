b0dafb99cef927da531e6914483f499bc40077b8
darektoa <darektoa@gmail.com>
2022-03-13 05:04:34 +0700

    Add: Request fields validation in store method of media API controller

--- app/Http/Controllers/Api/MediaController.php
+++ app/Http/Controllers/Api/MediaController.php
@@ -8,11 +8,21 @@ use App\Exceptions\ErrorException;
 use App\Helpers\ResponseHelper;
 use App\Helpers\StorageHelper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class MediaController extends Controller
 {
     public function store(Request $request) {
         try{
+            $validator = Validator::make($request->all(), [
+                'upload'    => 'required|file|max:30720',
+            ]);
+
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable', 422, $errors);
+            }
+
             $media  = $request->upload;
             $token  = $request->token;
             $token  = LoginToken::with('user.admin')
