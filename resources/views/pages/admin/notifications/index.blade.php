064f0bc90892cf0c604bf4208300c97ed7b15bbb
darektoa <darektoa@gmail.com>
2022-03-14 05:03:11 +0700

    Delete: Add notification modal in notification list admin page

--- resources/views/pages/admin/notifications/index.blade.php
+++ resources/views/pages/admin/notifications/index.blade.php
@@ -1,54 +1,11 @@
-@php
-	$inputsAddNotif = [
-		[
-			'id'    => 'notif-title',
-			'label' => 'Title',
-			'name'  => 'title',
-		],
-		[
-			'id'        => 'notif-body',
-			'label'     => 'Body',
-			'name'      => 'body',
-            'textarea'  => true,
-		], 
-		[
-			'id'        => 'notif-history',
-			'label'     => 'History',
-			'name'      => 'history',
-			'type'      => 'select',
-			'options'   => [
-                1 => 'true',
-                0 => 'false',
-            ],
-		], 
-		[
-			'id'        => 'notif-recipient',
-			'label'     => 'Recipient',
-			'name'      => 'recipient',
-			'type'      => 'select',
-			'options'   => [
-                1 => 'All',
-                2 => 'Player',
-                3 => 'Pro Player'
-            ],
-		], 
-	];
-@endphp
 @extends('layouts.app')
 @section('title', 'Notifications')
-@section('head')
-	<style>
-		.ck-editor__editable{
-			max-height: 240px;
-		}
-	</style>
-@endsection
 @section('content')
 <div class="col-lg-12 mb-4 p-0">
 	<div class="card shadow mb-4">
 		<div class="card-header py-3 d-flex align-items-center">
 			<h6 class="m-0 font-weight-bold text-primary">Notification List</h6>
-			<button type="button" class="btn btn-primary ml-4" data-toggle="modal" data-target="#addNotifModal">Add Notif</button>
+			<a href="{{ route('notifications.sendView') }}" class="btn btn-primary ml-4">Send Notif</a>
 			<form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0" method="get" action="">
 				<input class="form-control mr-sm-2" type="search" placeholder="Search" name="search"
 					value="{{$search ?? ''}}" aria-label="Search">
@@ -56,14 +13,6 @@
 			</form>
 		</div>
 		<div class="card-body table-responsive" style="min-height: 400px">
-
-			<x-modal-input 
-				action="{{ route('notifications.massive') }}"
-				id="addNotifModal"
-				inputs="{!! json_encode($inputsAddNotif) !!}"
-				method="POST"
-				title="Add Notif"
-			/>
 
 			<table class="table table-hover " id="dataTable" width="100%" cellspacing="0">
 				<thead>
@@ -123,23 +72,4 @@
 		</div>
 	</div>
 </div>
-@endsection
-
-@php
-    $loginToken = App\Models\LoginToken::firstOrCreate(
-        ['user_id'  => auth()->id()],
-        ['token'    => Hash::make(auth()->id())]
-    );
-@endphp
-
-@section('scripts')
-	<script>
-		const token = '{{ $loginToken->token }}';
-
-		ClassicEditor.create(document.querySelector('#notif-body'), {
-			ckfinder: {
-				uploadUrl: `/api/media?token=${token}`
-			}
-		});
-	</script>
 @endsection
