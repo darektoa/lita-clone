6e4605af241248b214c3ef94a158c7350532c5f7
darektoa <darektoa@gmail.com>
2022-03-15 05:05:21 +0700

    Add: Status name attribute in pro player service model

--- app/Models/ProPlayerService.php
+++ app/Models/ProPlayerService.php
@@ -20,4 +20,19 @@ class ProPlayerService extends Model
     public function service() {
         return $this->belongsTo(Service::class);
     }
+
+
+    public function getStatusNameAttribute() {
+        $statusName = null;
+
+        switch($this->status) {
+            case 0: $statusName = 'Pending'; break;
+            case 1: $statusName = 'Rejected'; break;
+            case 2: $statusName = 'Approved'; break;
+            case 3: $statusName = 'Banned'; break;
+            default: $statusName = 'Unknown';
+        }
+
+        return $statusName;
+    }
 }
