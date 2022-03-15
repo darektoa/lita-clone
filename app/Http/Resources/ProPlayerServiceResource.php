babd4bf8168519bcdbd65f7024196c7b1336e8f1
darektoa <darektoa@gmail.com>
2022-03-16 05:02:26 +0700

    Add: Pro player service resource

---
+++ app/Http/Resources/ProPlayerServiceResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProPlayerServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $playerLoaded   = $this->relationLoaded('player');
+        $userLoaded     = $playerLoaded ? $this->player->relationLoaded('user') : false;
+
+        return [
+            'id'                            => $this->id,
+            'rate'                          => $this->rate,
+            'bio'                           => $this->bio,
+            'status'                        => $this->status,
+            'status_name'                   => $this->status_name,
+            'activity'                      => $this->activity,
+            'activity_name'                 => $this->activity_name,
+            'user'                          => UserResource::make($this->when($userLoaded, $this->player->user->load('player'))),
+        ];
+    }
+}
