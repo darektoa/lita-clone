d73389761b479dc2cd435b1f88ec2615c1a4eec9
darektoa <darektoa@gmail.com>
2022-03-16 05:03:18 +0700

    Add: Index method in pro player service API controller

--- app/Http/Controllers/Api/ProPlayerServiceController.php
+++ app/Http/Controllers/Api/ProPlayerServiceController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ProPlayerServiceResource;
+use App\Models\ProPlayerService;
 use Illuminate\Http\Request;
 
 class ProPlayerServiceController extends Controller
 {
-    //
+    public function index() {
+        $services = ProPlayerService::with(['player.user'])
+            ->paginate(10);
+
+        return ResponseHelper::make($services);
+    }
 }
