0ba8a5868917ba80e756621aff2acb9fa0de4d77
darektoa <darektoa@gmail.com>
2022-03-17 05:04:29 +0700

    Add: Service resource

---
+++ app/Http/Resources/ServiceResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Helpers\StorageHelper;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ServiceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id'    => $this->id,
+            'icon'  => StorageHelper::url($this->icon),
+            'name'  => $this->name,
+        ];
+    }
+}
