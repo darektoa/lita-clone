2df056a4229b7b53c7048cccc2f98b51fcc1b1ed
darektoa <darektoa@gmail.com>
2022-03-17 05:02:19 +0700

    Add: Index method in service API controller

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index() {
+        $services = Service::orderBy('name', 'asc')->get();
+
+        return ResponseHelper::make($services);
+    }
 }
