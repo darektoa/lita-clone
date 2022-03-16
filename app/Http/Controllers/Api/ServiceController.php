3f65a69e0f3b8f88edbd95e9134eb404caa09b61
darektoa <darektoa@gmail.com>
2022-03-17 05:05:37 +0700

    Add: Using service resource in index method of service API controller

--- app/Http/Controllers/Api/ServiceController.php
+++ app/Http/Controllers/Api/ServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ServiceResource;
 use App\Models\Service;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,8 @@ class ServiceController extends Controller
     public function index() {
         $services = Service::orderBy('name', 'asc')->get();
 
-        return ResponseHelper::make($services);
+        return ResponseHelper::make(
+            ServiceResource::collection($services)
+        );
     }
 }
