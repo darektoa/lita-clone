f5e653cbd8e194067d6116a5fbc07a53afbbd57b
darektoa <darektoa@gmail.com>
2022-03-18 05:03:54 +0700

    Add: Filter services by gender according to logged in user in index method of pro player service API controller

--- app/Http/Controllers/Api/ProPlayerServiceController.php
+++ app/Http/Controllers/Api/ProPlayerServiceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\CollectionHelper;
 use App\Helpers\ResponseHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ProPlayerServiceResource;
@@ -11,8 +12,19 @@ use Illuminate\Http\Request;
 class ProPlayerServiceController extends Controller
 {
     public function index() {
-        $services = ProPlayerService::with(['player.user', 'service'])
-            ->paginate(10);
+        $user       = auth()->user() ?? null;
+        $userGender = $user->gender->id ?? null;
+        $services   = ProPlayerService::with(['player.user.gender', 'service'])
+            ->where('status', 2);
+
+        if($userGender === 1){
+            $services = $services->get()->sortByDesc(fn($query) => (
+                $query->player->user->gender_id === 2 // Female
+            ));
+            $services = CollectionHelper::paginate($services, 10);
+        } else {
+            $services = $services->get()->shuffle();
+        }
 
         return ResponseHelper::make(
             ProPlayerServiceResource::collection($services)
