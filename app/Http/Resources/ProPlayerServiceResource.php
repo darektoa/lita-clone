0ad7d7d51bcecdd7ab9a4bf6b0de6b846183180f
darektoa <darektoa@gmail.com>
2022-03-20 05:03:42 +0700

    Add: Price permatch property in pro player service resource

--- app/Http/Resources/ProPlayerServiceResource.php
+++ app/Http/Resources/ProPlayerServiceResource.php
@@ -18,15 +18,16 @@ class ProPlayerServiceResource extends JsonResource
         $userLoaded     = $playerLoaded ? $this->player->relationLoaded('user') : false;
 
         return [
-            'id'                            => $this->id,
-            'rate'                          => $this->rate,
-            'bio'                           => $this->bio,
-            'status'                        => $this->status,
-            'status_name'                   => $this->status_name,
-            'activity'                      => $this->activity,
-            'activity_name'                 => $this->activity_name,
-            'service'                       => ServiceResource::make($this->whenLoaded('service')),
-            'user'                          => UserResource::make($this->when($userLoaded, $this->player->user->load('player'))),
+            'id'                => $this->id,
+            'rate'              => $this->rate,
+            'bio'               => $this->bio,
+            'status'            => $this->status,
+            'status_name'       => $this->status_name,
+            'activity'          => $this->activity,
+            'activity_name'     => $this->activity_name,
+            'price_permatch'    => $this->price_permatch,
+            'service'           => ServiceResource::make($this->whenLoaded('service')),
+            'user'              => UserResource::make($this->when($userLoaded, $this->player->user->load('player'))),
         ];
     }
 }
