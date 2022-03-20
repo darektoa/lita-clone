07c76ed884ad4f476401b0c313c4a874ce63b6b7
darektoa <darektoa@gmail.com>
2022-03-21 05:03:15 +0700

    Add: Using soft deletes in pro player service model

--- app/Models/ProPlayerService.php
+++ app/Models/ProPlayerService.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProPlayerService extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $appends = [
         'activity_name',
