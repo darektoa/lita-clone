a3c7811426294a9dfb50a84a9d81c5df7d717d1b
darektoa <darektoa@gmail.com>
2022-03-30 05:05:28 +0700

    Add: Reporter_id field in reports table migration

--- database/migrations/2022_02_19_001451_create_reports_table.php
+++ database/migrations/2022_02_19_001451_create_reports_table.php
@@ -15,6 +15,7 @@ class CreateReportsTable extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('reporter_id')->constrained('users');
             $table->morphs('reportable');
             $table->text('report');
             $table->smallInteger('status')->default(0);
