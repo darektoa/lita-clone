73f11de971b1e607811459a142b9d23f3b76b7b1
darektoa <darektoa@gmail.com>
2022-04-02 05:05:32 +0700

    Add: Index method in service web controller

--- app/Http/Controllers/Web/ServiceController.php
+++ app/Http/Controllers/Web/ServiceController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function index() {
+        $services = Service::orderBy('name', 'asc')
+            ->paginate(10);
+
+        return view('pages.admin.setting.services.index', compact('services'));
+    }
 }
