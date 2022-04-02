26895b4782e2290b310994c6cf9b4feceed38de3
darektoa <darektoa@gmail.com>
2022-04-03 05:04:56 +0700

    Add: Update method in service web controller

--- app/Http/Controllers/Web/ServiceController.php
+++ app/Http/Controllers/Web/ServiceController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Helpers\StorageHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Service;
+use Exception;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ServiceController extends Controller
 {
@@ -13,5 +16,39 @@ class ServiceController extends Controller
             ->paginate(10);
 
         return view('pages.admin.setting.services.index', compact('services'));
+    }
+
+
+    public function update(Request $request, $serviceId) {
+        $service = Service::find($serviceId);
+
+        try{
+            $this->validate($request, [
+                'name'           => 'required|max:255',
+                'icon'           => 'nullable|image|max:2048',
+                'price'          => 'required|numeric|digits_between:0,18',
+                'player_revenue' => 'required|numeric|min:0',
+            ]);
+
+            if(!$service) throw new Exception('service not found');
+            
+            $service->name           = $request->name;
+            $service->price          = $request->price;
+            $service->player_revenue = $request->player_revenue;
+
+            if($request->icon) {
+                $iconPath = StorageHelper::put('images/service-icons', $request->icon);
+                StorageHelper::delete($service->icon);
+                $service->icon = $iconPath;
+            }
+
+            $service->update();
+            Alert::success('Success', 'Service edited successfully');
+        }catch(Exception $err) {
+            $errMessage = $err->getMessage();
+            Alert::error('Failed', $errMessage);
+        }finally{
+            return back();
+        }
     }
 }
