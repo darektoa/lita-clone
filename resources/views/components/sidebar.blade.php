42fbe70052dcad3d741f15680b7c508870a206cb
darektoa <darektoa@gmail.com>
2022-04-04 05:04:51 +0700

    Add: Services sub menu in setting navigation of sidebar component

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -30,13 +30,14 @@
   ];
 
   $settingNav = [
-    'Banners' => route('setting.banners.index'),
-    'Coins'   => route('setting.coins.index'),
-    'Faqs'    => route('setting.faqs.index'),
-    'Games'   => route('setting.games.index'),
-    'General' => route('setting.general.index'),
-    'Genders' => route('setting.genders.index'),
-    'Tiers'   => route('setting.tiers.index'),
+    'Banners'   => route('setting.banners.index'),
+    'Coins'     => route('setting.coins.index'),
+    'Faqs'      => route('setting.faqs.index'),
+    'Games'     => route('setting.games.index'),
+    'General'   => route('setting.general.index'),
+    'Genders'   => route('setting.genders.index'),
+    'Services'  => route('setting.services.index'),
+    'Tiers'     => route('setting.tiers.index'),
   ];
 @endphp
 
