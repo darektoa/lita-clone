36c073fb9605b28a5510827fe462d0ed359e8457
darektoa <darektoa@gmail.com>
2022-04-06 05:05:16 +0700

    Fix: Report method in player API controller

--- app/Http/Controllers/Api/PlayerController.php
+++ app/Http/Controllers/Api/PlayerController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Exceptions\ErrorException;
 use App\Models\User;
-use App\Helpers\ResponseHelper;
+use App\Helpers\{ResponseHelper, StorageHelper};
 use App\Http\Controllers\Controller;
-use App\Http\Resources\UserResource;
+use App\Http\Resources\{ReportResource, UserResource};
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PlayerController extends Controller
 {
@@ -14,5 +16,45 @@ class PlayerController extends Controller
         return ResponseHelper::make(
             UserResource::make($user->load('player'))
         );
+    }
+
+    public function report(Request $request, User $user) {
+        try{
+            $validator = Validator::make($request->all(), [
+                'report' => 'required',
+                'proof'  => 'nullable|file'
+            ]);
+
+            $reportData = [
+                'reporter_id'   => auth()->id(),
+                'report'        => $request->report,
+                'status'        => 0,
+            ];
+
+            if($validator->fails()) {
+                $errors = $validator->errors()->all();
+                throw new ErrorException('Unprocessable', 422, $errors);
+            }
+
+            if($user->id === auth()->id())
+                throw new ErrorException("Not allowed, Can't report yourself", 403);
+
+            if($request->proof) {
+                $proofPath = StorageHelper::put('media/reports', $request->proof);
+                $reportData['proof'] = $proofPath;
+            }
+
+            $report = $user->reports()->create($reportData);
+
+            return ResponseHelper::make(
+                ReportResource::make($report)
+            );
+        }catch(ErrorException $err) {
+            return ResponseHelper::error(
+                $err->getErrors(),
+                $err->getMessage(),
+                $err->getCode(),
+            );
+        }
     }
 }
