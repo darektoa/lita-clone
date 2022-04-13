2872dfa867a9401ed0728a952299b4ace143212f
darektoa <darektoa@gmail.com>
2022-04-14 05:03:34 +0700

    Edit: Reportable field data type using uuid morphs

--- database/migrations/2022_02_19_001451_create_reports_table.php
+++ database/migrations/2022_02_19_001451_create_reports_table.php
@@ -16,7 +16,7 @@ class CreateReportsTable extends Migration
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
             $table->foreignId('reporter_id')->constrained('users');
-            $table->morphs('reportable');
+            $table->uuidMorphs('reportable');
             $table->text('report');
             $table->text('proof')->nullable();
             $table->smallInteger('status')->default(0);
