8220882842fa45460bd7a0974cd236133330dde6
darektoa <darektoa@gmail.com>
2022-04-19 05:02:53 +0700

    Add: Index method in report web controller

--- app/Http/Controllers/Web/ReportController.php
+++ app/Http/Controllers/Web/ReportController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Report;
 use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-    //
+    public function index() {
+        $reports = Report::latest()->paginate(10);
+
+        return view('pages.admin.reports.index', compact('reports'));
+    }
 }
